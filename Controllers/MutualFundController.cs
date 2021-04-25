cdfb56e5f33f3e0cc5a7131c14d7b4c162941da5
arijit54 <duttaarijit54@gmail.com>
2021-04-25 17:48:59 +0530

    initialCommit

---
+++ Controllers/MutualFundController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MutualFund.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MutualFund.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MutualFundController : ControllerBase
+    {
+        private IMutualFundDb MutualFundDbObj;
+
+        public MutualFundController(IMutualFundDb obj)
+        {
+            MutualFundDbObj = obj;
+        }
+        [HttpGet]
+        public ActionResult<MutualFundDb> GetMutualFunds()
+        {
+            var temp = new List<dynamic>();
+            foreach(var x in MutualFundDb.mutualFundDetailList)
+            {
+                x.ToString();
+                temp.Add(x);
+            }
+            if (temp != null)
+                return Ok(temp);
+            else
+                return BadRequest();
+        }
+    }
+}
---
+++ Models/MutualFundDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MutualFund.Models
+{
+    public class MutualFundDb : IMutualFundDb
+    {
+        public static List<MutualFundDetail> mutualFundDetailList = new List<MutualFundDetail> {
+            new MutualFundDetail { MutualFundId = 1, MutualFundName = "Axis Mutual Funds",MutualFundValue=100},
+            new MutualFundDetail { MutualFundId = 2, MutualFundName = "Reliance Mutual Funds", MutualFundValue = 200 },
+            new MutualFundDetail { MutualFundId = 3, MutualFundName = "SBI Mutual Funds", MutualFundValue = 300 },
+            new MutualFundDetail { MutualFundId = 4, MutualFundName = "ICICI Mutual Funds", MutualFundValue = 400 },
+            new MutualFundDetail { MutualFundId = 5, MutualFundName = "UTI Mutual Funds", MutualFundValue = 500 }
+            };
+
+        public MutualFundDetail MutualFundNav(string mutualFundName)
+        {
+            return mutualFundDetailList.FirstOrDefault(c => c.MutualFundName.ToLower() == mutualFundName.ToLower());
+
+        }
+    }
+}
